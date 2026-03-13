53b9d9d9ba0902cf24e805675a3de8f65b27d39f
Supriya-004 <supriyawalhekar08@gmail.com>
2026-03-13 08:58:52 +0530

    js function added

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+function displayTodos(){
+    const list = document.getElementById("todoList");
+    list.innerHTML = "";
+
+    todos.forEach((todo,index)=>{
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+        ${todo}
+        <span class="delete" onclick="deleteTodo(${index})">X</span>
+        `;
+
+        list.appendChild(li);
+    });
+}
 
 function addTodo(){
     const input = document.getElementById("todoInput");

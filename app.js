6e683c9cb4d7c0119ebec62c8025c00e9c93ee02
Supriya-004 <supriyawalhekar08@gmail.com>
2026-03-13 09:00:05 +0530

    js fuction

--- app.js
+++ app.js
@@ -16,20 +16,13 @@ function displayTodos(){
     });
 }
 
-function addTodo(){
-    const input = document.getElementById("todoInput");
-    const value = input.value.trim();
 
-    if(value === ""){
-        alert("Please enter a task");
-        return;
-    }
-
-    todos.push(value);
+function deleteTodo(index){
+    todos.splice(index,1);
 
     localStorage.setItem("todos", JSON.stringify(todos));
 
-    input.value="";
     displayTodos();
 }
 
+displayTodos();

2632dccececd9c4effe0aa277119646a57a40901
Supriya-004 <supriyawalhekar08@gmail.com>
2026-03-13 09:00:29 +0530

    add to to function

--- app.js
+++ app.js
@@ -17,6 +17,23 @@ function displayTodos(){
 }
 
 
+function addTodo(){
+    const input = document.getElementById("todoInput");
+    const value = input.value.trim();
+
+    if(value === ""){
+        alert("Please enter a task");
+        return;
+    }
+
+    todos.push(value);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    input.value="";
+    displayTodos();
+}
+
 function deleteTodo(index){
     todos.splice(index,1);
 
